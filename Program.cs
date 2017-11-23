8bc1ab1d22b9b1eb78d7b026ddd2ab9a5dabcfe1
AnAndrey <an.andrey.g@gmail.com>
2017-11-23 11:41:53 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ProjectA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Some uniqe code of project A");
+            SayHello();
+            
+        }
+
+        static void SayHello()
+        {
+            Console.WriteLine("Hello guys");
+        }
+    }
+}
