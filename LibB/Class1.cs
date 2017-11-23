004fab7764fa6a3f181b4b4faf56177092bb63e7
AnAndrey <an.andrey.g@gmail.com>
2017-11-23 11:43:51 +0300

    Add project files.

---
+++ LibB/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LibB
+{
+    public class Class1
+    {
+        public void HelloFromB()
+        {
+            Console.WriteLine("Helllo from project B");
+        }
+    }
+}
